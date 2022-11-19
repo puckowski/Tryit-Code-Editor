4af74b75ffc3fc2f6790f0c5917d70c205299484
Daniel Puckowski <puckowski.d@gmail.com>
2022-11-19 17:59:18 -0500

    Update build version
    * Update build version.

--- src/tryit/stores/global.store.js
+++ src/tryit/stores/global.store.js
@@ -11,7 +11,7 @@ class StoreGlobal {
         this.inlineHeight = '';
         this.collapsedMode = false;
         this.showPreview = false;
-        this.version = '4.2';
+        this.version = '4.3';
         this.showHelp = false;
         this.sourceHasNewInput = BehaviorSubject(false);
         this.invalidScriptIndices = BehaviorSubject([]);
