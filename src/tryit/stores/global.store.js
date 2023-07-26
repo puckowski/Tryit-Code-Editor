2a1e519994679a4e7d85c988410caa149abce23f
Daniel Puckowski <puckowski.d@gmail.com>
2023-07-26 17:09:38 -0400

    Update code editor version to 5.7
    * Update code editor version to 5.7.

--- src/tryit/stores/global.store.js
+++ src/tryit/stores/global.store.js
@@ -12,7 +12,7 @@ class StoreGlobal {
         this.inlineHeight = '';
         this.collapsedMode = false;
         this.showPreview = false;
-        this.version = '5.6';
+        this.version = '5.7';
         this.showHelp = false;
         this.sourceHasNewInput = BehaviorSubject(false);
         this.invalidScriptIndices = BehaviorSubject([]);
